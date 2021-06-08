f501536b1e9efd166447e0310e69631ab01527d8
AkA <cer4k4@gmail.com>
2021-06-08 21:53:17 +0430

    insert from db to redis

--- import.go
+++ import.go
@@ -1,17 +1,65 @@
 package main
 
 import (
-        "log"
-	 "database/sql"
-	 _ "github.com/go-sql-driver/mysql"
+	"context"
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-
-func main() {
-	db, err := sql.Open("mysql", "golang:golang123@(localhost)/golang_test")
-	if err != nil{
-		panic(err.Error())
-	}
-	defer db.Close()
-	log.Println(db)
+type Message struct {
+	Sender     string `json:"sender"`
+	Receiver   string `json:"receiver"`
+	ChatRoomID int    `json:"chat_room_id"`
+	Message    string `json:"message"`
+	Type       string `json:"type"`
+	File       string `json:"file"`
+	gorm.Model
 }
+var DB *gorm.DB
+var DBErr error
+
+func Connection() {
+	DB, DBErr = gorm.Open("mysql", "golang:golang123@(localhost)/golang_test?charset=utf8&parseTime=True&loc=Local")
+	if DBErr != nil {
+		log.Println(DBErr)
+	}
+}
+
+func main(){
+	data := GetMessage()
+	ctx := context.Background()
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		Password: "",
+		DB: 0,
+	})
+	result, err := client.HSet(ctx,"message:"+strconv.Itoa(data.ChatRoomID),"sender",data.Sender,"receiver",data.Receiver,"chatroom",data.ChatRoomID,"message",data.Message,"type",data.Type,"file","","create_at",data.CreatedAt,"update_at",data.UpdatedAt,"delete_at","").Result()
+	if err != nil{
+		log.Println(err)
+	}
+	fmt.Println(result)
+	fmt.Println(data.Receiver)
+
+//	data := GetAllMessage()
+//	fmt.Println(data)
+}
+
+
+func GetMessage() Message{
+	Connection()
+	var message Message
+	DB.First(&message,1386)
+	return message
+}
+func GetAllMessage() interface{} {
+	Connection()
+	messages := []Message{}
+	DB.Find(&messages)
+	return messages
+}
+
